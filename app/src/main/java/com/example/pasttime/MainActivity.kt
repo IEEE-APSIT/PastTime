bf43e018673c22e0018fff8c4bee00f58be853a4
anujssmishra <anujssmishra@gmail.com>
2020-07-29 22:23:23 +0530

    Added knob in the home page

--- app/src/main/java/com/example/pasttime/MainActivity.kt
+++ app/src/main/java/com/example/pasttime/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.pasttime
 
-import androidx.appcompat.app.AppCompatActivity
+//import android.R
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import it.beppi.knoblibrary.Knob
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val knob = findViewById(R.id.knob) as Knob
+        knob.state = 1
+        knob.setOnStateChanged {
+            // do something
+        }
     }
 }
